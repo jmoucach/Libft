314365c6d3082c56c7c16647a9cd1c8f76f0b584
Jean-pierre MOUCACHEN <jmoucach@student.42.fr>
2018-12-21 13:49:42 +0100

    lib

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoucach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/16 10:30:30 by jmoucach          #+#    #+#             */
+/*   Updated: 2018/12/06 15:31:34 by jmoucach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 50000
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
